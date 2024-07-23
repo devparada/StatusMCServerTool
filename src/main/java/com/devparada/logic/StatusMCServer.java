4eea745bebb82525b0252f2f95b9da260d1f9f24
Raúl <raul.parada040103@gmail.com>
2024-07-23 23:58:20 +0200

    Add license in StatusMCServer

--- src/main/java/com/devparada/logic/StatusMCServer.java
+++ src/main/java/com/devparada/logic/StatusMCServer.java
@@ -1,6 +1,18 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * This file is part of StatusMCServerTool.
+ *
+ * StatusMCServerTool is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * StatusMCServerTool is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received showDataFrame copy of the GNU General Public License
+ * along with StatusMCServerTool. If not, see <https://www.gnu.org/licenses/>.
  */
 package com.devparada.logic;
 
